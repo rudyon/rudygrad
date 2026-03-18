327188d382bdbe004253cbd29f7c94c670fc69b0
rudyon <rudyon@example.com>
2026-03-19 01:23:33 +0300

    Cache parameters in MLP/Layer/Neuron

--- src/nn.rs
+++ src/nn.rs
@@ -4,17 +4,20 @@ use rand::distr::{Distribution, Uniform};
 pub struct Neuron {
     pub w: Vec<Value>,
     pub b: Value,
+    pub params: Vec<Value>,
 }
 
 impl Neuron {
     pub fn new(nin: usize) -> Self {
         let mut rng = rand::rng();
         let range = Uniform::new(-1.0f32, 1.0f32).unwrap();
-        let w = (0..nin)
+        let w: Vec<Value> = (0..nin)
             .map(|_| Value::new(range.sample(&mut rng)))
             .collect();
         let b = Value::new(range.sample(&mut rng));
-        Neuron { w, b }
+        let mut params = w.clone();
+        params.push(b.clone());
+        Neuron { w, b, params }
     }
 
     pub fn call(&self, x: &[Value]) -> Value {
@@ -22,41 +25,43 @@ impl Neuron {
     }
 
     pub fn parameters(&self) -> Vec<Value> {
-        let mut p = self.w.clone();
-        p.push(self.b.clone());
-        p
+        self.params.clone()
     }
 }
 
 pub struct Layer {
     pub neurons: Vec<Neuron>,
+    pub params: Vec<Value>,
 }
 
 impl Layer {
     pub fn new(nin: usize, nout: usize) -> Self {
-        let neurons = (0..nout).map(|_| Neuron::new(nin)).collect();
-        Layer { neurons }
+        let neurons: Vec<Neuron> = (0..nout).map(|_| Neuron::new(nin)).collect();
+        let params = neurons.iter().flat_map(|n| n.parameters()).collect();
+        Layer { neurons, params }
     }
     pub fn call(&self, x: &[Value]) -> Vec<Value> {
         self.neurons.iter().map(|n| n.call(x)).collect()
     }
     pub fn parameters(&self) -> Vec<Value> {
-        self.neurons.iter().flat_map(|n| n.parameters()).collect()
+        self.params.clone()
     }
 }
 
 pub struct MLP {
     pub layers: Vec<Layer>,
+    pub params: Vec<Value>,
 }
 
 impl MLP {
     pub fn new(nin: usize, nouts: Vec<usize>) -> Self {
         let mut sz = vec![nin];
         sz.extend(nouts);
-        let layers = (0..sz.len() - 1)
+        let layers: Vec<Layer> = (0..sz.len() - 1)
             .map(|i| Layer::new(sz[i], sz[i+1]))
             .collect();
-        MLP { layers }
+        let params = layers.iter().flat_map(|l| l.parameters()).collect();
+        MLP { layers, params }
     }
     pub fn call(&self, mut x: Vec<Value>) -> Vec<Value> {
         for layer in &self.layers {
@@ -65,10 +70,10 @@ impl MLP {
         x
     }
     pub fn parameters(&self) -> Vec<Value> {
-        self.layers.iter().flat_map(|l| l.parameters()).collect()
+        self.params.clone()
     }
     pub fn zero_grad(&self) {
-        for p in self.parameters() {
+        for p in &self.params {
             p.0.borrow_mut().grad = 0.0;
         }
     }
